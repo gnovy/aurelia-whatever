3ed4784ae9d6d17d00017a05d335d85fc74c05cf
gnovy <thegnovy@gmail.com>
2017-06-14 19:04:56 +0800

    add home and routing

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router){
+    config.map([
+      { route: '', moduleId: 'home/home', title: 'Aurelia Whatever'},
+      { route: 'login', moduleId: 'login/login', title: 'Login'},
+      { route: 'register', moduleId: 'register/register', title: 'Register'},
+    ]);
+
+    this.router = router;
+  }
 }
